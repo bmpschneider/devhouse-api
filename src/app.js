72282cf555fe9259e47b3741743ccd342b28de4e
bmpschneider <brunopschneider@gmail.com>
2021-03-02 23:23:45 -0300

    Connect database

--- src/app.js
+++ src/app.js
@@ -1,10 +1,18 @@
 import express from 'express';
+import mongoose from 'mongoose';
 import  routes from './routes';
 
 class App{
   
   constructor(){
     this.server = express();
+
+    mongoose.connect('mongodb+srv://devhouse:devhouse@devhouse.6b3ci.mongodb.net/devhouse?retryWrites=true&w=majority',{
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
+    
+    
     this.middlewares();
     this.routes();
   }
